8c8bfaae3a292e146f71a6485d52e79d83fc931d
zsiyed <69491969+zsiyed@users.noreply.github.com>
2023-05-06 17:12:03 -0700

    finished pt 1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,48 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    let voices = [];
+    speechSynthesis.addEventListener("voiceschanged", () => {
+      voices = speechSynthesis.getVoices();
+      populateVoiceSelect(voices);
+    });
+
+    // Populate the "Select Voice" dropdown with available voices
+    function populateVoiceSelect(voices) {
+        const voiceSelect = document.getElementById("voice-select");
+        voiceSelect.innerHTML = "";
+        voices.forEach((voice) => {
+        const option = document.createElement("option");
+        option.textContent = voice.name;
+        option.setAttribute("value", voice.name);
+        voiceSelect.appendChild(option);
+        });
+    }
+  
+  // Handle button click to initiate speech synthesis
+    const button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        const textarea = document.getElementById("text-to-speak");
+        const voiceSelect = document.getElementById("voice-select");
+        const selectedVoiceName = voiceSelect.value;
+        const selectedVoice = voices.find((voice) => voice.name === selectedVoiceName);
+    
+        // Create new SpeechSynthesisUtterance with selected text and voice
+        const utterance = new SpeechSynthesisUtterance(textarea.value);
+        utterance.voice = selectedVoice;
+    
+        // Swap image to open mouth while speaking
+        const image = document.querySelector("img");
+        utterance.addEventListener("start", () => {
+        image.src = "assets/images/smiling-open.png";
+        });
+    
+        // Swap image back to smiling when speaking ends
+        utterance.addEventListener("end", () => {
+        image.src = "assets/images/smiling.png";
+        });
+    
+        // Speak text with selected voice
+        speechSynthesis.speak(utterance);
+    });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,44 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // Get the  DOM elements
+const hornSelect = document.getElementById("horn-select");
+const soundImage = document.querySelector("img");
+const volumeSlider = document.getElementById("volume");
+const volumeImage = document.querySelector("#volume-controls img");
+const playButton = document.querySelector("button");
+const audioElement = document.querySelector("audio");
+const jsConfetti = new JSConfetti();
+
+
+// Set up event listeners
+hornSelect.addEventListener("change", () => {
+  soundImage.src = `assets/images/${hornSelect.value}.svg`;
+
+  audioElement.src = `assets/audio/${hornSelect.value}.mp3`;
+});
+
+volumeSlider.addEventListener("input", () => {
+  // Update the volume of the audio element based on the slider value
+  audioElement.volume = volumeSlider.value / 100;
+
+  // Update the volume icon based on the slider value
+  if (volumeSlider.value == 0) {
+    volumeImage.src = "assets/icons/volume-level-0.svg";
+  } else if (volumeSlider.value < 33) {
+    volumeImage.src = "assets/icons/volume-level-1.svg";
+  } else if (volumeSlider.value < 67) {
+    volumeImage.src = "assets/icons/volume-level-2.svg";
+  } else {
+    volumeImage.src = "assets/icons/volume-level-3.svg";
+  }
+});
+
+playButton.addEventListener("click", () => {
+  // Play the audio element at the selected volume
+  audioElement.play();
+  if (hornSelect.value === "party-horn") {
+        jsConfetti.addConfetti();
+      }
+});
 }
